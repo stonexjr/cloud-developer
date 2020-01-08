5a1ed4e8ebc261cb4ca96ea659aa928ab77e746f
Jinrong Xie <jrxie@ucdavis.edu>
2020-01-07 22:41:58 -0800

    updated script to run docker-compose

--- course-03/exercises/udacity-c3-deployment/docker/run_docker.sh
+++ course-03/exercises/udacity-c3-deployment/docker/run_docker.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/zsh
-docker run -p 80:80 --name reverseproxy \
---rm stonexjr/nginx:alphine
+#docker run -p 80:80 --name reverseproxy \
+#--rm stonexjr/nginx:alphine
+
+# build multiple docker images
+docker-compose -f docker-compose-build.yaml build --parallel
+# push multiple docker images
+docker-compose -f docker-compose-build.yaml push
+#launch multiple containers
+docker-compose -f docker-compose.yaml up
